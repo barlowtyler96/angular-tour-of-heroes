8bf64fbec96d3325351a6957df02e08494efaca9
Tyler Barlow <barlowtyler96@gmail.com>
2023-11-07 17:45:55 -0500

    add heroes component

---
+++ src/app/heroes/dist/heroes.component.js
@@ -0,0 +1,38 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.HeroesComponent = void 0;
+var core_1 = require("@angular/core");
+var HeroesComponent = /** @class */ (function () {
+    function HeroesComponent(heroService, messageService) {
+        this.heroService = heroService;
+        this.messageService = messageService;
+        this.heroes = [];
+    }
+    HeroesComponent.prototype.ngOnInit = function () {
+        this.getHeroes();
+    };
+    HeroesComponent.prototype.onSelect = function (hero) {
+        this.selectedHero = hero;
+        this.messageService.add("HeroesComponent: Selected hero id=" + hero.id);
+    };
+    HeroesComponent.prototype.getHeroes = function () {
+        var _this = this;
+        this.heroService.getHeroes()
+            .subscribe(function (heroes) { return _this.heroes = heroes; });
+    };
+    HeroesComponent = __decorate([
+        core_1.Component({
+            selector: 'app-heroes',
+            templateUrl: './heroes.component.html',
+            styleUrls: ['./heroes.component.css']
+        })
+    ], HeroesComponent);
+    return HeroesComponent;
+}());
+exports.HeroesComponent = HeroesComponent;
