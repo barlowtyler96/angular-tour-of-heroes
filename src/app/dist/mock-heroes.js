246b32f6c029dbaadc049970e40e9e36ea9e14b8
Tyler Barlow <barlowtyler96@gmail.com>
2023-11-07 17:44:11 -0500

    add mock data

---
+++ src/app/dist/mock-heroes.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+exports.HEROES = void 0;
+exports.HEROES = [
+    { id: 12, name: 'Dr. Nice' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magenta' },
+    { id: 16, name: 'Rubberman' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr. IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' },
+];
